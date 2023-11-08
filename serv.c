73e25a1dc56ebf8132473cbd740015938b1414a3
SuperGmaX <54249107+supergmax@users.noreply.github.com>
2023-11-09 00:09:58 +0100

    Create serv.c
    test

---
+++ serv.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Content-type: text/html\n\n");
+    printf("<html><body>");
+    printf("<h1>Interface utilisateur CGI en C</h1>");
+    // Ajoute le reste de ton HTML ici
+    printf("</body></html>");
+
+    return 0;
+}
